2447366a60cb934b74940936550133b8cb9e15d5
Sam Bentley <samjamesbentley@gmail.com>
2021-05-12 14:18:46 +1000

    add handling for multiple datacenters

--- Move-VMCrossVC.ps1
+++ Move-VMCrossVC.ps1
@@ -2,12 +2,17 @@ function Move-VMCrossVC {
     <#
     .Synopsis
     This function will move all virtual machines from one host to another host in different vCenter Servers via Cross vCenter vMotion.
-    It is assumed that all dvSwitch Port Proups have identical names on the source and destination virtual switches and datastores and datastore clusters are also identical.
-    Folders on the source and destination must be unique to avoid virtual machines being moved to the incorrect folder. 
 
-    If you'd like to move only a single virtual machine specify the -SingleVM parameter and the name of a virtual machine.
+    It is assumed that all dvSwitch Port Proups have identical names on the source and destination virtual switches, datastores, 
+    datastore clusters and datacenters have identical names. 
+    
+    Folders on the source and destination must be unique to avoid virtual machines being moved to the incorrect folder, with the 
+    exception of folder names being the same over multiple datacenters. The function will check for uniqueness. 
 
-    For testing, pass the -DryRun parameter and the function will enable -WhatIf on the Move-VM task. 
+    If you'd like to move only a single virtual machine specify the SingleVM parameter and the name of a virtual machine. This is 
+    helpful for testing before you move entire host. 
+
+    For testing, pass the DryRun parameter and the function will pass the WhatIf parameter on the Move-VM task. 
     .EXAMPLE
     Move-VMCrossVC -SourceHost esxi02.test.lab -DestHost esxi03.test.lab -sourcevCenter vc01 -DestvCenter vc02 -DestDVSwitch dswitch
     #>
@@ -74,34 +79,40 @@ function Move-VMCrossVC {
         }    
     }
     else {
-        $vms = get-vmhost $sourcehost | get-vm | where-object {$_.Name -notmatch "vCLS"} # get all virtual machines on source host, exclude vCLS on vSphere 7.0
+        $vms = get-vmhost $sourcehost | get-vm | where-object { $_.Name -notmatch "vCLS" } # get all virtual machines on source host, exclude vCLS on vSphere 7.0
     }
 
     # Check to see if virtual machines were found
-    if ($vms -eq $null){
+    if ($vms -eq $null) {
         Write-Host -foregroundcolor Red "ERROR: No virtual machines were found with the specified parameters."
         exit
     }
-    # Check both vCenter Servers for duplicate folder names
+    # Check DataCenters in both vCenter Servers for duplicate folder names
     Write-Host -foregroundcolor Green "Checking if both vCenter Servers have unique VM folder names..."
-    $vCenterServers = "$SourcevCenter","$DestvCenter"
+    $vCenterServers = "$SourcevCenter", "$DestvCenter"
     foreach ($server in $vCenterServers) {
-        $AllFolders = get-folder -server $server | where-object Type -Match "VM" 
-        $UniqueFolders = get-folder -server $server | where-object Type -Match "VM" | select-object -unique
-        $Compare = Compare-Object -ReferenceObject $AllFolders -DifferenceObject $UniqueFolders
-        If ($null -eq $Compare) {
-            write-host "- Duplicate folders not found on $server"
-        }
-        Else {
-            write-host -foregroundcolor Red "Duplicate folders found on $server, exiting..."
-            write-host "To avoid virtual machines ending up in the wrong folder, folder names must be unique!"
-            exit
+        $dcs = get-datacenter -server $server
+        foreach ($dc in $dcs) {
+            $AllFolders = get-folder -location (get-datacenter $dc -server $server) | where-object Type -Match "VM" 
+            $UniqueFolders = get-folder -location (get-datacenter $dc -server $server) | where-object Type -Match "VM" | select-object -unique
+            $Compare = Compare-Object -ReferenceObject $AllFolders -DifferenceObject $UniqueFolders
+            If ($null -eq $Compare) {
+                write-host "- Duplicate folders not found on $server in $dc"
+
+            }
+            Else {
+                write-host -foregroundcolor Red "Duplicate folders found on $server in $dc, exiting..."
+                write-host "To avoid virtual machines ending up in the wrong folder, folder names must be unique!"
+                write-host "Found folders:"
+                $AllFolders.parent
+                exit
+            }
         }
     }
 
     # Execute Move-VM for each virtual machine in $vms
-    if ($DryRun) {Write-Host -foregroundcolor Magenta "Dry run enabled, passing -WhatIf to Move-VM, no virtual machines will be moved"}
-    else {Write-Host -foregroundcolor Green "Beginning Cross vCenter vMotion of virtual machines..."}
+    if ($DryRun) { Write-Host -foregroundcolor Magenta "Dry run enabled, passing -WhatIf to Move-VM, no virtual machines will be moved" }
+    else { Write-Host -foregroundcolor Green "Beginning Cross vCenter vMotion of virtual machines..." }
     foreach ($vm in $vms) {
         $SourceNetworkAdapters = get-networkadapter $vm # get current vm network adapter 
         # Since datastore clusters are unsupported for Cross VC vMotion, get the datastore with the most amount of free space
@@ -120,14 +131,17 @@ function Move-VMCrossVC {
         $DestPortGroups = @()
         foreach ($networkAdapter in $SourceNetworkAdapters) {
             $PortGroup = get-vdportgroup -name $NetworkAdapter.NetworkName -server $DestvCenter -VDSwitch $DestdvSwitch
-            Write-Host "Moving"  $networkAdapter.name "from" $networkAdapter.NetworkName "to" $PortGroup
+            Write-Host "- Moving"  $networkAdapter.name "from" $networkAdapter.NetworkName "to" $PortGroup
             $DestPortGroups += $PortGroup
         }
-        # Execute Move-VM using What-If if DrynRun specified then move the virtual machine to it's original VM folder location 
+        # Execute Move-VM using What-If if DryRun specified then move the virtual machine to it's original VM folder location 
+        # Includes handling for multiple DataCenters where folders may have the same names (ie. Production in DC1 and Production in DC2)
         move-vm -vm $vm -NetworkAdapter $SourceNetworkAdapters -PortGroup $DestPortGroups -destination $VMHost -datastore $Datastore -whatif:$DryRun | Out-Null
         if ($DryRun -eq $false) {
-            Write-Host "Moving" $vm.name "to folder:" $vm.Folder.Name
-            get-vm $vm.name -server $DestvCenter | move-vm -InventoryLocation $vm.folder.Name | Out-Null
+            $vmfolder = get-vm $vm.name -server $DestvCenter | get-datacenter -server $DestvCenter | get-folder $vm.folder.Name
+            $datacenter = get-vm $vm.name -server $DestvCenter | get-datacenter
+            Write-Host "- Moving" $vm.name "to folder in DC:" $vm.Folder.Name "in" $datacenter.Name
+            get-vm $vm.name -server $DestvCenter | move-vm -InventoryLocation $vmfolder | Out-Null
         }
     }
 } 
